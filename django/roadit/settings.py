5965819a132845ab230a66da1b5c2abcd9aa0608
Dezeli <haterecursive@gmail.com>
2025-03-27 17:31:01 +0900

    NGINX Setting - Django 테스트 완료 - Windows를 위한 bat 파일 생성 - Mac을 위한 sh 파일 생성

--- django/roadit/settings.py
+++ django/roadit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*v57mng2mhhul+qe@ylv&-*cqe*-ego==6a)8$+hfngmhccbes
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
